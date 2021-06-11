31cc2df587c7b7c91aac80b2fa6a7d278288b2f2
Daniel Mueller <danielmueller@uwalumni.com>
2021-06-11 07:17:07 -0400

    Updates

---
+++ precache-manifest.cd05725a82180896b83be086a93e58ec.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bb2472e0a3a0e29c1a4",
+    "url": "/levelled-nback/static/js/app.41e2ccbe.chunk.js"
+  },
+  {
+    "revision": "1e7a04d97356f6f30f00",
+    "url": "/levelled-nback/static/js/runtime~app.31765fd3.js"
+  },
+  {
+    "revision": "10928148cd0c2123414a",
+    "url": "/levelled-nback/static/js/2.8953b877.chunk.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/levelled-nback/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/levelled-nback/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "e766963327e0a89f9ec2ba88646b6177",
+    "url": "/levelled-nback/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/levelled-nback/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/levelled-nback/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/levelled-nback/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/levelled-nback/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/levelled-nback/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b3263095df30cb7db78c613e73f9499a",
+    "url": "/levelled-nback/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/levelled-nback/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "8ef52a15e44481b41e7db3c7eaf9bb83",
+    "url": "/levelled-nback/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/levelled-nback/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "/levelled-nback/expo-service-worker.js"
+  },
+  {
+    "revision": "aecac02ac8e85179dd1161464f747c23",
+    "url": "/levelled-nback/index.html"
+  },
+  {
+    "revision": "f3f84d97c5466e60dcc39db93306aa12",
+    "url": "/levelled-nback/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/levelled-nback/serve.json"
+  },
+  {
+    "revision": "e7a7b3e23d2275a5b0fade360ad89d40",
+    "url": "/levelled-nback/static/js/2.8953b877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1071922eac3f222004bfae59f9ac1b9a",
+    "url": "/levelled-nback/favicon-16.png"
+  },
+  {
+    "revision": "f0334eeab9fa4156a9ff752d9db28829",
+    "url": "/levelled-nback/favicon-32.png"
+  },
+  {
+    "revision": "0f873b563a39cb37f7af1c71e9a136e9",
+    "url": "/levelled-nback/favicon.ico"
+  },
+  {
+    "revision": "38aa852e3e2a96706932774defb03a22",
+    "url": "/levelled-nback/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/levelled-nback/expo-service-worker.js",{scope:"/levelled-nback/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
