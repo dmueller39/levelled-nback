f85a016029efd9a2a6ca02952abe2bbca3f70a80
Daniel Mueller <danielmueller@uwalumni.com>
2022-05-09 17:27:02 -0400

    Updates

--- register-service-worker.js
+++
@@ -1,1 +0,0 @@
-"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/levelled-nback/expo-service-worker.js",{scope:"/levelled-nback/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
